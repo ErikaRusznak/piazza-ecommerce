10fed095be8939482e399378cf46bce42d707e3f
Erika Rusznak <erika.rusznak@gmail.com>
2023-11-19 19:05:51 +0200

    connect a cart to a new registered user

--- src/main/java/com/ozius/internship/project/security/jwt/JwtAuthenticationResource.java
+++ src/main/java/com/ozius/internship/project/security/jwt/JwtAuthenticationResource.java
@@ -28,8 +28,6 @@ public class JwtAuthenticationResource {
     @PostMapping("/authenticate")
     public JwtTokenResponse authenticate(@RequestBody JwtTokenRequest jwtTokenRequest){
 
-//        UserDetails userDetails = databaseUserDetailsService.loadUserByUsername(jwtTokenRequest.username()); //not needed, just for future to create scopes
-
         var authenticationToken =
                 new UsernamePasswordAuthenticationToken(
                         jwtTokenRequest.username(),
