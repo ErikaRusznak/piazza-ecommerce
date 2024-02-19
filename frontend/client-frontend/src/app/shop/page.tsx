c9fdb97af4011d32a4374476020bd3a9ba47d429
Erika Rusznak <erika.rusznak@gmail.com>
2024-02-19 22:17:43 +0200

    Add pagination and num of page selector

--- frontend/client-frontend/src/app/shop/page.tsx
+++ frontend/client-frontend/src/app/shop/page.tsx
@@ -2,7 +2,7 @@
 
 import React, {useEffect, useState} from "react";
 import MainLayout from "@/components/templates/MainLayout";
-import {Box, List, ListItem, Typography} from "@mui/material";
+import {Box, List, ListItem, Pagination, Typography} from "@mui/material";
 import useTheme from "@/theme/themes";
 import {useRouter} from "next/navigation";
 import {getProductsApi} from "../../../api/entities/ProductApi";
@@ -10,6 +10,7 @@ import ProductCard from "@/components/moleculas/ProductCard";
 import MainProductList from "@/components/organisms/product/MainProductList";
 import SearchComponent from "@/components/moleculas/filtering/SearchComponent";
 import FilteringComponent from "@/components/organisms/filtering/FilteringComponent";
+import NumberOfPageSelect from "@/components/atoms/filtering/NumberOfPageSelect";
 
 const buildFilterOptionsFromQueryParams = (queryParams) => {
     return {
@@ -27,28 +28,30 @@ const ProductsPage = () => {
     const router = useRouter();
     const [products, setProducts] = useState([]);
 
-    const [itemsPerPage, setItemsPerPage] = useState(12);
-    // const [totalNumberProducts, setTotalNumberProducts] = useState(0);
+    const [itemsPerPage, setItemsPerPage] = useState(8);
+    const [totalNumberProductsPerPage, setTotalNumberProductsPerPage] = useState(0);
     const [currentPage, setCurrentPage] = useState(1);
-    //
-    // const [productSort, setProductSort] = useState({criteria: null, orderSort: null});
-    //
-    // const [queryParams, setQueryParams] = useState(new URLSearchParams(location.search));
-    //
-    // const [filterOptions, setFilterOptions] = useState(buildFilterOptionsFromQueryParams(new URLSearchParams(location.search)));
-    //
-    // const [isModalOpen, setIsModalOpen] = useState(false);
-    //
-    // const [productId, setProductId] = useState(null);
-    //
+
+    const [productSort, setProductSort] = useState({criteria: null, orderSort: null});
+
+    const [queryParams, setQueryParams] = useState(new URLSearchParams(location.search));
+
+    const [filterOptions, setFilterOptions] = useState(buildFilterOptionsFromQueryParams(new URLSearchParams(location.search)));
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const [productId, setProductId] = useState(null);
+
     const [isLoading, setLoading] = useState(true)
 
+    const [totalNumberOfProducts, setTotalNumberOfProducts] = useState(0);
     const getProducts = (page, newItemsPerPage, sortSpecs, filterSpecs) => {
-        // setItemsPerPage(newItemsPerPage);
+        setItemsPerPage(newItemsPerPage);
         getProductsApi(page, newItemsPerPage, sortSpecs, filterSpecs)
             .then((res) => {
                 setProducts(res.data.data);
-                // setTotalNumberProducts(res.data.numberOfElements);
+                setTotalNumberOfProducts(res.data.numberOfElements);
+                setTotalNumberProductsPerPage(res.data.numberOfElements);
                 setLoading(false)
 
             })
@@ -56,120 +59,147 @@ const ProductsPage = () => {
     }
 
     useEffect(() => {
-        // const filterSpecs = buildFilterSpecs();
-        // const sortSpecs = buildSortSpecs();
-        // setOrRemoveQueryParameters(filterOptions);
+        const filterSpecs = buildFilterSpecs();
+        const sortSpecs = buildSortSpecs();
+        setOrRemoveQueryParameters(filterOptions);
         getProducts(currentPage, itemsPerPage, [], []);
     }, [/*filterOptions, productSort,*/ itemsPerPage, currentPage]);
 
     // useEffect(() => {
     //     navigate({search: queryParams.toString()});
     // }, [queryParams]);
-    //
-    // useEffect(() => {
-    //     setFilterOptions(buildFilterOptionsFromQueryParams(new URLSearchParams(location.search)));
-    // }, [location.search]);
-    //
+
+    useEffect(() => {
+        setFilterOptions(buildFilterOptionsFromQueryParams(new URLSearchParams(location.search)));
+    }, [location.search]);
+
     // useEffect(() => {
     //     setCurrentPage(1);
     // }, [breakpoint]);
 
-    // const handleItemsPerPageChange = (event) => {
-    //     const newItemsPerPage = parseInt(event.target.value);
-    //     const filterSpecs = buildFilterSpecs();
-    //     const sortSpecs = buildSortSpecs();
-    //     setCurrentPage(1);
-    //     getProducts(1, newItemsPerPage, sortSpecs, filterSpecs);
-    // }
-    //
-    // const handleSortChanged = (sortFilter) => {
-    //     setProductSort(sortFilter);
-    //     setCurrentPage(1);
-    // }
-    // const handleOnFilterChanged = (newFilterOptions) => {
-    //     setFilterOptions(newFilterOptions);
-    //     setCurrentPage(1);
-    // }
-    //
-    // const createFilterCriteria = (criteria, operation, value) => {
-    //     return `${criteria}[${operation}]${value}`;
-    // }
-    // const createSortCriteria = (criteria, orderSort) => {
-    //     return `${criteria}-${orderSort}`;
-    // }
-    //
-    // const createValueForFilterCriteria = (filterOption) => {
-    //     if (Array.isArray(filterOption)) {
-    //         if (filterOption.length > 1) {
-    //             return filterOption.join('|');
-    //         } else if (filterOption.length === 1) {
-    //             return filterOption[0];
-    //         } else {
-    //             return null;
-    //         }
-    //     } else {
-    //         return filterOption;
-    //     }
-    // };
-    //
-    // const buildFilterSpecs = () => {
-    //     const filterSearchSpec = [];
-    //     if (filterOptions.categoryName.length) {
-    //         const value = createValueForFilterCriteria(filterOptions.categoryName);
-    //         filterSearchSpec.push(createFilterCriteria("categoryName", "eq", value));
-    //     }
-    //     if (filterOptions.cityName.length) {
-    //         const value = createValueForFilterCriteria(filterOptions.cityName);
-    //         filterSearchSpec.push(createFilterCriteria("cityName", "eq", value));
-    //     }
-    //     if (filterOptions.priceFrom) {
-    //         filterSearchSpec.push(createFilterCriteria("priceFrom", "gte", filterOptions.priceFrom));
-    //     }
-    //     if (filterOptions.priceTo) {
-    //         filterSearchSpec.push(createFilterCriteria("priceTo", "lte", filterOptions.priceTo));
-    //     }
-    //     if (filterOptions.productName) {
-    //         filterSearchSpec.push(createFilterCriteria("productName", "starts_with", filterOptions.productName));
-    //     }
-    //     return filterSearchSpec;
-    // }
-    //
-    // const buildSortSpecs = () => {
-    //     const sortSpecs = [];
-    //     if (productSort.criteria && productSort.orderSort) {
-    //         sortSpecs.push(createSortCriteria(productSort.criteria, productSort.orderSort));
-    //     }
-    //     return sortSpecs;
-    // }
-    //
-    // const setOrRemoveQueryParameters = (filterOptions) => {
-    //     const newQueryParams = new URLSearchParams();
-    //
-    //     for (let key in filterOptions) {
-    //         const value = filterOptions[key];
-    //         if (value !== null && value !== '' && (Array.isArray(value) ? value.length > 0 : true)) {
-    //             setSearchQueryParameters(key, value, newQueryParams);
-    //         } else {
-    //             newQueryParams.delete(key);
-    //         }
-    //     }
-    //     setQueryParams(newQueryParams);
-    // }
-    //
-    //
-    // const setSearchQueryParameters = (key, value, newQueryParams) => {
-    //     if (Array.isArray(value)) {
-    //         for (let val in value) {
-    //             newQueryParams.append(key, value[val]);
-    //         }
-    //     } else {
-    //         newQueryParams.set(key, value);
-    //     }
-    // }
-    //
+    const handleItemsPerPageChange = (event) => {
+        const newItemsPerPage = parseInt(event.target.value);
+        const filterSpecs = buildFilterSpecs();
+        const sortSpecs = buildSortSpecs();
+        setCurrentPage(1);
+        getProducts(1, newItemsPerPage, sortSpecs, filterSpecs);
+    }
+
+    const handleSortChanged = (sortFilter) => {
+        setProductSort(sortFilter);
+        setCurrentPage(1);
+    }
+    const handleOnFilterChanged = (newFilterOptions) => {
+        setFilterOptions(newFilterOptions);
+        setCurrentPage(1);
+    }
+
+    const createFilterCriteria = (criteria, operation, value) => {
+        return `${criteria}[${operation}]${value}`;
+    }
+    const createSortCriteria = (criteria, orderSort) => {
+        return `${criteria}-${orderSort}`;
+    }
+
+    const createValueForFilterCriteria = (filterOption) => {
+        if (Array.isArray(filterOption)) {
+            if (filterOption.length > 1) {
+                return filterOption.join('|');
+            } else if (filterOption.length === 1) {
+                return filterOption[0];
+            } else {
+                return null;
+            }
+        } else {
+            return filterOption;
+        }
+    };
+
+    const buildFilterSpecs = () => {
+        const filterSearchSpec = [];
+        if (filterOptions.categoryName.length) {
+            const value = createValueForFilterCriteria(filterOptions.categoryName);
+            filterSearchSpec.push(createFilterCriteria("categoryName", "eq", value));
+        }
+        if (filterOptions.cityName.length) {
+            const value = createValueForFilterCriteria(filterOptions.cityName);
+            filterSearchSpec.push(createFilterCriteria("cityName", "eq", value));
+        }
+        if (filterOptions.priceFrom) {
+            filterSearchSpec.push(createFilterCriteria("priceFrom", "gte", filterOptions.priceFrom));
+        }
+        if (filterOptions.priceTo) {
+            filterSearchSpec.push(createFilterCriteria("priceTo", "lte", filterOptions.priceTo));
+        }
+        if (filterOptions.productName) {
+            filterSearchSpec.push(createFilterCriteria("productName", "starts_with", filterOptions.productName));
+        }
+        return filterSearchSpec;
+    }
+
+    const buildSortSpecs = () => {
+        const sortSpecs = [];
+        if (productSort.criteria && productSort.orderSort) {
+            sortSpecs.push(createSortCriteria(productSort.criteria, productSort.orderSort));
+        }
+        return sortSpecs;
+    }
+
+    const setOrRemoveQueryParameters = (filterOptions) => {
+        const newQueryParams = new URLSearchParams();
+
+        for (let key in filterOptions) {
+            const value = filterOptions[key];
+            if (value !== null && value !== '' && (Array.isArray(value) ? value.length > 0 : true)) {
+                setSearchQueryParameters(key, value, newQueryParams);
+            } else {
+                newQueryParams.delete(key);
+            }
+        }
+        setQueryParams(newQueryParams);
+    }
+
+
+    const setSearchQueryParameters = (key, value, newQueryParams) => {
+        if (Array.isArray(value)) {
+            for (let val in value) {
+                newQueryParams.append(key, value[val]);
+            }
+        } else {
+            newQueryParams.set(key, value);
+        }
+    }
+
+    const numberOfPages = Math.ceil(totalNumberOfProducts / itemsPerPage);
 
     return (
         <MainLayout>
+            <Pagination
+                count={numberOfPages} //total num of pages
+                defaultPage={1}
+                page={currentPage} // current page
+                variant="outlined"
+                color="primary"
+                onChange={(e, page) => {
+                    setCurrentPage(page);
+                }}
+                sx={{
+                    mb: 3,
+                    '& .MuiPaginationItem-root': {
+                        color: theme.palette.info.main,
+                        border: '1px solid #93B1A6',// Default color for unselected items
+                    },
+                    '& .Mui-selected': {
+                        backgroundColor: theme.palette.secondary.main, // Background color for selected item
+                        color: theme.palette.info.main, // Text color for selected item
+                    },
+                }}
+            />
+            <NumberOfPageSelect
+                itemsPerPage={itemsPerPage}
+                setItemsPerPage={setItemsPerPage}
+                handleItemsPerPageChange={handleItemsPerPageChange}
+            />
             <Box sx={{
                 maxWidth: "872px",
                 margin: "0 auto",
@@ -189,11 +219,15 @@ const ProductsPage = () => {
                 <Typography variant="h4" sx={{color: theme.palette.info.main}}>
                     Check the products
                 </Typography>
-                <FilteringComponent />
+                <FilteringComponent
+                    filterOptions={filterOptions}
+                    onFilterChanged={handleOnFilterChanged}
+                    onSortChanged={handleSortChanged}/>
             </Box>
             <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
                 <MainProductList products={products}/>
             </Box>
+
         </MainLayout>
     );
 };
---
+++ frontend/client-frontend/src/components/atoms/filtering/NumberOfPageSelect.tsx
@@ -0,0 +1,70 @@
+import React, {useEffect} from "react";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import useTheme from "@/theme/themes";
+import {Box, useMediaQuery} from "@mui/material";
+
+const NumberOfPageSelect = ({itemsPerPage, setItemsPerPage, handleItemsPerPageChange}) => {
+
+    const theme = useTheme();
+    // const breakpoint = useBreakpoint();
+    //
+    // useEffect(() => {
+    //     setItemsPerPage(12);
+    // }, [breakpoint]);
+    const isXSmallScreen = useMediaQuery(theme.breakpoints.between('xxs', 'xs'));
+    const isSmallScreen = useMediaQuery(theme.breakpoints.between('xs', 'sm'));
+    const isMediumScreen = useMediaQuery(theme.breakpoints.between('sm', 'md'));
+    const isLargeScreen = useMediaQuery(theme.breakpoints.between('md', 'lg'));
+    const isXLargeScreen = useMediaQuery(theme.breakpoints.between('lg', 'xl'));
+    const isXXLargeScreen = useMediaQuery(theme.breakpoints.up('xl'));
+
+    return (
+        // <Box sx={{ maxWidth: 200 }}>
+            <FormControl fullWidth sx={{maxWidth: 200}} size={"small"}>
+                <InputLabel
+                    id="demo-simple-select-label"
+                    sx={{
+                        color: theme.palette.primary.main,
+                        marginBottom: 1,
+                        '&.Mui-focused': {
+                            color: theme.palette.primary.main,
+                        },
+                    }}
+                >
+                    Items per page</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={itemsPerPage}
+                    label="Items per page"
+                    onChange={handleItemsPerPageChange}
+                    sx={{
+                        '& .MuiSelect-select': {
+                            backgroundColor: theme.palette.secondary.main,
+                            color: theme.palette.info.main,
+                        },
+                        '& .MuiOutlinedInput-notchedOutline': {
+                            borderColor: theme.palette.primary.main,
+                        },
+
+                    }}
+                >
+                    {!isXSmallScreen && (
+                        <MenuItem value="6" sx={{color: "red"}}>6 per page</MenuItem>
+                    )}
+                    {!isSmallScreen && (
+                        <MenuItem value="8">8 per page</MenuItem>
+                    )}
+                    {!isMediumScreen && (
+                        <MenuItem value="12">12 per page</MenuItem>
+                    )}
+                </Select>
+            </FormControl>
+        // </Box>
+    );
+};
+
+export default NumberOfPageSelect;
