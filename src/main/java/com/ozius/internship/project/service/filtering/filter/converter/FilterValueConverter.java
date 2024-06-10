46f2500319e21e39f137554683263ef5b42165c4
Erika <erika.rusznak@gmail.com>
2024-06-10 20:48:24 +0300

    Delete unused classes

--- src/main/java/com/ozius/internship/project/service/filtering/filter/converter/FilterValueConverter.java
+++
@@ -1,5 +0,0 @@
-package com.ozius.internship.project.service.filtering.filter.converter;
-
-public interface FilterValueConverter<O> {
-    O convert(String value);
-}
--- src/main/java/com/ozius/internship/project/service/filtering/filter/converter/NoTransformationConverter.java
+++
@@ -1,8 +0,0 @@
-package com.ozius.internship.project.service.filtering.filter.converter;
-
-public class NoTransformationConverter implements FilterValueConverter<String>{
-    @Override
-    public String convert(String value) {
-        return value;
-    }
-}
